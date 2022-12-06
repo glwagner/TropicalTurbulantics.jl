4577fe59e295ed5bdbbfec4a80231c228102f35c
Gregory Wagner <wagner.greg@gmail.com>
2022-12-06 11:05:18 -0700

    Update the plot

--- plot_single_colum_simulation.jl
+++ plot_single_colum_simulation.jl
@@ -20,22 +20,33 @@ T = interior(Tt, 1, 1, :, :)
 S = interior(St, 1, 1, :, :)
 e = interior(et, 1, 1, :, :)
 Ri = interior(Rit, 1, 1, :, :)
+Ri⁻¹ = 1 ./ interior(Rit, 1, 1, :, :)
 
-fig = Figure()
+umax = max(maximum(abs, u), maximum(abs, v))
+ulim = 3umax / 4
+
+fig = Figure(resolution=(1800, 1200))
 
 ax_u = Axis(fig[1, 1], xlabel="Time (days)", ylabel="z (m)")
 ax_v = Axis(fig[1, 2], xlabel="Time (days)", ylabel="z (m)")
 ax_T = Axis(fig[2, 1], xlabel="Time (days)", ylabel="z (m)")
 ax_S = Axis(fig[2, 2], xlabel="Time (days)", ylabel="z (m)")
 ax_e = Axis(fig[3, 1], xlabel="Time (days)", ylabel="z (m)")
-ax_Ri = Axis(fig[3, 2], xlabel="Time (days)", ylabel="z (m)")
+ax_R = Axis(fig[3, 2], xlabel="Time (days)", ylabel="z (m)")
 
-heatmap!(ax_u,  t ./ days, z, permutedims(u))
-heatmap!(ax_v,  t ./ days, z, permutedims(v))
-heatmap!(ax_T,  t ./ days, z, permutedims(T))
-heatmap!(ax_S,  t ./ days, z, permutedims(S))
-heatmap!(ax_e,  t ./ days, z, permutedims(e), colorrange=(0, 1e-4))
-heatmap!(ax_Ri, t ./ days, z_Ri, permutedims(Ri))
+hm_u = heatmap!(ax_u, t ./ days, z, permutedims(u), colormap=:balance, colorrange=(-ulim, ulim))
+hm_v = heatmap!(ax_v, t ./ days, z, permutedims(v), colormap=:balance, colorrange=(-ulim, ulim))
+hm_T = heatmap!(ax_T, t ./ days, z, permutedims(T), colormap=:thermal)
+hm_S = heatmap!(ax_S, t ./ days, z, permutedims(S), colormap=:haline)
+hm_e = heatmap!(ax_e, t ./ days, z, permutedims(e), colormap=:solar, colorrange=(0, 1e-4))
+hm_R = heatmap!(ax_R, t ./ days, z_Ri, permutedims(Ri⁻¹), colorrange=(0, 5))
+
+Colorbar(fig[1, 0], hm_u, flipaxis=false, label="Zonal velocity (m s⁻¹)")
+Colorbar(fig[1, 3], hm_v, label="Meridional velocity (m s⁻¹)")
+Colorbar(fig[2, 0], hm_T, flipaxis=false, label="Temperature (ᵒC)")
+Colorbar(fig[2, 3], hm_S, label="Salinity (g kg⁻¹)")
+Colorbar(fig[3, 0], hm_e, flipaxis=false, label="Turbulent kinetic energy (m s⁻¹)")
+Colorbar(fig[3, 3], hm_R, label="1 / Ri")
 
 display(fig)
 
